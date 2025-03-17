b4967d9f7178415fcfbbc3cc572e6ed2f8bf8c1a
Thada Srisaluaikun <gnar875@gmail.com>
2025-03-17 15:51:25 +0700

    re config uploadthing middleware

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,28 +1,36 @@
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+function authenticateUser() {
+  const user = auth();
+  if (!user) throw new Error(`No user found in system`);
+  return user;
+}
 
 const f = createUploadthing();
 
-// function getUser() {
-//   const user = auth();
-//   if (!user) throw new Error("Unauthorize");
-//   return user;
-// }
-
 export const ourFileRouter = {
-  foodImage: f(["image/png", "image/jpeg"]).onUploadComplete(() => {}),
+  foodImage: f(["image/png", "image/jpeg"])
+    .middleware(authenticateUser)
+    .onUploadComplete(() => {}),
   subaccountLogo: f({
     image: { maxFileSize: "4MB", maxFileCount: 1 },
-  }).onUploadComplete(() => {}),
+  })
+    .middleware(authenticateUser)
+    .onUploadComplete(() => {}),
   avatar: f({
     image: { maxFileSize: "4MB", maxFileCount: 1 },
-  }).onUploadComplete(() => {}),
+  })
+    .middleware(authenticateUser)
+    .onUploadComplete(() => {}),
   agencyLogo: f({
     image: { maxFileSize: "4MB", maxFileCount: 1 },
-  }).onUploadComplete(() => {}),
-  media: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } }).onUploadComplete(
-    () => {}
-  ),
+  })
+    .middleware(authenticateUser)
+    .onUploadComplete(() => {}),
+  media: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(authenticateUser)
+    .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
