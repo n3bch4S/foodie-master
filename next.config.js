369cb53aadad3459399f455e5c0f3b79c1618900
Thada Srisaluaikun <gnar875@gmail.com>
2025-03-17 17:16:04 +0700

    config next image hostname

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "uploadthing.com",
-      "utfs.io",
-      "img.clerk.com",
-      "subdomain",
-      "files.stripe.com",
-      "io9y7kt0tw.ufs.sh",
-    ],
-  },
+  images: { remotePatterns: [{ hostname: "*.ufs.sh" }] },
   reactStrictMode: false,
 };
 
