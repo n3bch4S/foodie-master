baca8666b1e00cf0b91c3349095057f4b7dbd82c
Thada Srisaluaikun <gnar875@gmail.com>
2025-03-17 15:51:56 +0700

    next config to allow uploadthing domain

--- next.config.js
+++ next.config.js
@@ -1,20 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-        port: "",
-        pathname: "**",
-      },
-    ],
     domains: [
       "uploadthing.com",
       "utfs.io",
       "img.clerk.com",
       "subdomain",
       "files.stripe.com",
+      "io9y7kt0tw.ufs.sh",
     ],
   },
   reactStrictMode: false,
