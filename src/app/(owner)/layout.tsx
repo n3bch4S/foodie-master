d7e520a7835b61f00c94269ba22d43776f159380
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-04 01:00:17 +0700

    layout separator

--- src/app/(owner)/layout.tsx
+++ src/app/(owner)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/header";
+import { Separator } from "@/components/ui/separator";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({
@@ -9,6 +10,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <Header />
+      <Separator />
       {children}
     </ClerkProvider>
   );
