6daa3abe9b450a9e8e4fa17a7f0a564217495161
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-03 20:25:51 +0700

    use header in owner layout

--- src/app/(owner)/layout.tsx
+++ src/app/(owner)/layout.tsx
@@ -1,16 +1,5 @@
+import { Header } from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
-import { UserButton } from "@clerk/nextjs";
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
 
 export default function RootLayout({
   children,
@@ -19,18 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Link
-              </NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <Header />
       {children}
     </ClerkProvider>
   );
