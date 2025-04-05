78bfbc577686d9bbea9c72c227eae1ce13841a6b
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-06 01:30:16 +0700

    resform

--- src/components/form/restaurant-form.tsx
+++ src/components/form/restaurant-form.tsx
@@ -16,6 +16,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { UploadButton } from "@/lib/uploadthing";
 import { toast } from "sonner";
+import React, { useMemo, useState } from "react";
+import Image from "next/image";
+import { AspectRatio } from "../ui/aspect-ratio";
 
 const restaurantFormSchema = z.object({
   name: z.string().min(1, { message: "กรุณากรอกชื่อร้านอาหาร" }),
@@ -24,7 +27,12 @@ const restaurantFormSchema = z.object({
 });
 type RestaurantForm = z.infer<typeof restaurantFormSchema>;
 
+function toUTUrl(key: string) {
+  return `https://${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}.ufs.sh/f/${key}`;
+}
+
 export function RestaurantForm() {
+  const [logoName, setLogoName] = useState<string>("");
   const form = useForm<RestaurantForm>({
     resolver: zodResolver(restaurantFormSchema),
     defaultValues: {
@@ -33,6 +41,9 @@ export function RestaurantForm() {
       logoKey: "",
     },
   });
+  const logoKey = useMemo(() => {
+    return form.getValues().logoKey;
+  }, [form.getValues().logoKey]);
 
   function onSubmit(values: RestaurantForm) {
     console.log(values);
@@ -73,16 +84,39 @@ export function RestaurantForm() {
             </FormItem>
           )}
         />
-        <UploadButton
-          endpoint="restaurantLogo"
-          onClientUploadComplete={(res) => {
-            form.setValue("logoKey", res[0].key);
-            toast.success("อัปโหลดสำเร็จ");
-          }}
-          onUploadError={() => {
-            toast.error("อัปโหลดไม่สำเร็จ");
-          }}
-        />
+        <div className="flex gap-4 items-center justify-center">
+          <div className="w-32">
+            <AspectRatio>
+              {logoKey ? (
+                <Image
+                  src={toUTUrl(logoKey)}
+                  alt="โลโก้ร้านอาหาร"
+                  fill
+                  sizes="10vw"
+                  className="rounded-md object-cover border-4"
+                />
+              ) : (
+                <div className="w-full h-full border-4 rounded-md flex justify-center items-center text-slate-400">
+                  ไม่มีโลโก้
+                </div>
+              )}
+            </AspectRatio>
+          </div>
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <p>{logoName}</p>
+            <UploadButton
+              endpoint="restaurantLogo"
+              onClientUploadComplete={(res) => {
+                form.setValue("logoKey", res[0].key);
+                setLogoName(res[0].name);
+                toast.success("อัปโหลดสำเร็จ");
+              }}
+              onUploadError={() => {
+                toast.error("อัปโหลดไม่สำเร็จ");
+              }}
+            />
+          </div>
+        </div>
         <Button type="submit">บันทึก</Button>
       </form>
     </Form>
