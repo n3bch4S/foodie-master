7e52f3bedcdf022d616fa0e6656d7fbb6066a310
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-06 21:19:50 +0700

    qs

--- src/app/(owner)/foods/page.tsx
+++ src/app/(owner)/foods/page.tsx
@@ -1,14 +1,12 @@
-import { fetchFoods, FoodDetail } from "@/lib/food";
 import { DataTable } from "./data-table";
 import { foodColumns } from "./columns";
+import { fetchFoods } from "@/lib/food/index";
 
 export default async function Food() {
-  const foodData = await fetchFoods().catch(() => {
-    return [] as FoodDetail[];
-  });
+  const foodDetails = await fetchFoods();
   return (
     <div className="container flex flex-col h-screen">
-      <DataTable data={foodData} columns={foodColumns} />
+      <DataTable data={foodDetails} columns={foodColumns} />
     </div>
   );
 }
