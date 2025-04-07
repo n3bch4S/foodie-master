058db7df90f6b785ff12eacd6368810cf185083b
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-08 00:02:08 +0700

    qs

--- src/app/(owner)/editor/page.tsx
+++ src/app/(owner)/editor/page.tsx
@@ -1,15 +1,3 @@
 export default async function Editor() {
-  // const data = await currentUser()
-  //   .then((user) =>
-  //     user
-  //       ? getLatestPage(user.id, "8841ee40-e032-44cd-8cb9-462e331e96f1")
-  //       : Promise.reject(`Can't find user data`)
-  //   )
-  //   .then(async (pageDetail) => ({
-  //     dom: pageDetail.dom,
-  //     component: await renderComponent(pageDetail.dom),
-  //   }));
-
-  // return <DndCtx dom={{ id: "root", children: [] }}>hello</DndCtx>;
   return null;
 }
