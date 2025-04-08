2c9c0cd0a40b2dc099b1249fcee6446cc40f0f3a
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-08 22:02:32 +0700

    editor provider

--- src/providers/editor/editor-context.tsx
+++ src/providers/editor/editor-context.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { CUSTOM_PAGE_DOM } from "@/lib/page/constants";
+import { Dom } from "@/lib/page/types";
+import { DndContext, UniqueIdentifier } from "@dnd-kit/core";
 import { createContext, Dispatch, useContext, useReducer } from "react";
 
 type EditorContextType = {
@@ -6,6 +9,7 @@ type EditorContextType = {
   screenSize: "DESKTOP" | "MOBILE";
   isPreview: boolean;
   currentPage: string;
+  dom: Dom;
 };
 const EditorContext = createContext<EditorContextType | null>(null);
 
@@ -23,6 +27,7 @@ export function EditorProvider({ children }: EditorProviderProps) {
     screenSize: "DESKTOP",
     isPreview: false,
     currentPage: "Home",
+    dom: CUSTOM_PAGE_DOM,
   };
 
   const [editorContext, dispatch] = useReducer(editorReducer, initContext);
@@ -30,7 +35,7 @@ export function EditorProvider({ children }: EditorProviderProps) {
   return (
     <EditorContext.Provider value={editorContext}>
       <EditorDispatchContext.Provider value={dispatch}>
-        {children}
+        <DndContext>{children}</DndContext>
       </EditorDispatchContext.Provider>
     </EditorContext.Provider>
   );
@@ -45,8 +50,10 @@ export function useEditorDispatch(): EditorDispatchContextType {
 }
 
 export type EditorActionType = {
-  type: "changePage";
+  type: "changePage" | "updateDom";
   page?: string;
+  childId?: UniqueIdentifier;
+  newParentId?: UniqueIdentifier;
 };
 function editorReducer(
   editorContext: EditorContextType,
@@ -56,8 +63,66 @@ function editorReducer(
     case "changePage": {
       return { ...editorContext, currentPage: action.page! };
     }
+    case "updateDom": {
+      const newDom = moveComponent(
+        editorContext.dom,
+        action.childId!,
+        action.newParentId!
+      );
+      return { ...editorContext, dom: newDom };
+    }
     default: {
-      throw Error("Unknown action: " + action.type);
+      throw new Error("Unknown action: " + action.type);
     }
   }
+}
+
+function findIn(component: Dom, id: UniqueIdentifier): Dom | null {
+  if (component.id === id) return component;
+  if (component.children.length === 0) return null;
+  const foundChild = component.children.find(
+    (child) => findIn(child, id) !== null
+  );
+  if (foundChild) return findIn(foundChild, id);
+  return null;
+}
+
+function findParentIn(component: Dom, id: UniqueIdentifier): Dom | null {
+  if (component.children.length === 0) return null;
+  const foundChild = component.children.find((child) => child.id === id);
+  if (foundChild) return component;
+  const foundGrandChild = component.children.find(
+    (child) => findIn(child, id) !== null
+  );
+  if (foundGrandChild) return findParentIn(foundGrandChild, id);
+  return null;
+}
+
+function deepClone(component: Dom): Dom {
+  return {
+    ...component,
+    children: component.children.map(deepClone),
+  } as Dom;
+}
+
+function moveComponent(
+  component: Dom,
+  childId: UniqueIdentifier,
+  newParentId: UniqueIdentifier
+): Dom {
+  const newComponent = deepClone(component);
+  const oldParent = findParentIn(newComponent, childId);
+  if (!oldParent)
+    throw new Error(`Can't find parent of component id ${childId}`);
+  const newParent = findIn(newComponent, newParentId);
+  if (!newParent) throw new Error(`Can't find new parent id ${newParentId}`);
+  if (oldParent.id === newParent.id) return newComponent;
+  const child = findIn(oldParent, childId);
+  if (!child) throw new Error(`Can't find child with id ${childId}`);
+
+  newParent.children.push(child);
+  oldParent.children = oldParent.children.filter(
+    (child) => child.id !== childId
+  );
+  return newComponent;
 }
