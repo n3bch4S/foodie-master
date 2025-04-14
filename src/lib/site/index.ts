4c53a5ac457fb4fbe0750ad10f8ba14813c61f33
Thada Srisaluaikun <gnar875@gmail.com>
2025-04-14 19:51:11 +0700

    qs

--- src/lib/site/index.ts
+++ src/lib/site/index.ts
@@ -10,16 +10,16 @@ type SiteDetail = z.infer<typeof siteDetailSchema>;
 
 const db = new PrismaClient();
 
-export async function createSiteDetail(ownerId: string): Promise<SiteDetail> {
-  return db.site.create({ data: { ownerId } });
-}
+// export async function createSiteDetail(ownerId: string): Promise<SiteDetail> {
+//   return db.site.create({ data: { ownerId } });
+// }
 
-export async function getSiteDetail(ownerId: string): Promise<SiteDetail> {
-  return db.site
-    .findFirst({ where: { ownerId } })
-    .then((mightSite) =>
-      mightSite
-        ? mightSite
-        : Promise.reject(`Can't find site for owner ${ownerId}`)
-    );
-}
+// export async function getSiteDetail(ownerId: string): Promise<SiteDetail> {
+//   return db.site
+//     .findFirst({ where: { ownerId } })
+//     .then((mightSite) =>
+//       mightSite
+//         ? mightSite
+//         : Promise.reject(`Can't find site for owner ${ownerId}`)
+//     );
+// }
