d75a045869e49ebf427199d3e8f0c3a965c39e76
Thada Srisaluaikun <103327493+n3bch4S@users.noreply.github.com>
2025-04-21 00:30:08 +0700

    add goto page & redirect when not found rtr (#13)

--- src/app/(owner)/editor/page.tsx
+++ src/app/(owner)/editor/page.tsx
@@ -1,13 +1,19 @@
 import { getPages } from "@/lib/page";
 import { ClientPage } from "./_client";
 import { EditorProvider } from "@/providers/editor/editor-provider";
+import { getSite } from "@/lib/restaurant";
+import { redirect } from "next/navigation";
 
 export default async function Editor() {
   const pages = await getPages();
+  const site = await getSite().then((site) => {
+    if (!site) redirect("/restaurant");
+    return site;
+  });
 
   return (
     <EditorProvider>
-      <ClientPage pages={pages} />
+      <ClientPage pages={pages} site={site} />
     </EditorProvider>
   );
 }
